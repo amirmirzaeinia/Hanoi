2d2d91077a1de98c5a0cc35a959b4d2684962d97
Amir Mirzaeinia <mirzaeinia.amir@gmail.com>
2020-02-25 17:54:58 -0700

    Update Hanoi.py

--- Hanoi.py
+++ Hanoi.py
@@ -5,6 +5,27 @@ all the Hanoi conditions are applied.
 # R is number of rods
 # disks_distribution is an array that shows location of that disk. for example if R=3 and disks_distribution=[0,1,2], 
 #it means largest disk on rod 0, second largest on rod 1 and the smalest disk on rod 2.
+sample output for
+R=4 
+disks_distribution =[1,2,0,
+                     1,3,0,
+                     3,2,1,
+                     1,0,3,
+                     2]
+output=
+[[2, 5, 10], [0, 3, 8, 9], [1, 7, 12], [4, 6, 11]]
+[[2, 5, 10], [3, 8, 9], [0, 1, 7, 12], [4, 6, 11]]
+[[0, 1, 2, 5, 10], [3, 8, 9], [7, 12], [4, 6, 11]]
+[[5, 10], [0, 1, 2, 3, 8, 9], [7, 12], [4, 6, 11]]
+[[5, 10], [8, 9], [7, 12], [0, 1, 2, 3, 4, 6, 11]]
+[[0, 1, 2, 3, 4, 5, 10], [8, 9], [7, 12], [6, 11]]
+[[10], [8, 9], [7, 12], [0, 1, 2, 3, 4, 5, 6, 11]]
+[[10], [8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 12], [11]]
+[[10], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [12], [11]]
+[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [], [12], [11]]
+[[], [], [12], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
+[[], [], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], []]
+move_counter:7667
 '''
 
  
